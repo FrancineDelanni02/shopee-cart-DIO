14ee6ccf95210f15370e696dedd315346ae3e3a6
FrancineDelanni02 <delannifrancine@gmail.com>
2025-07-10 20:46:09 -0300

    update cart

---
+++ src/Cart.js
@@ -0,0 +1,9 @@
+//Update, Read, Create, Delete Items
+async function adicionarItem(item){
+    console.log("Adicionando item");   
+}
+
+async function exibirItem(idItem){
+    console.log("Exibindo item")
+}
+
